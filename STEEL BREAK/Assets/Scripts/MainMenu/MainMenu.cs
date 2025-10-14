3d8fccb46e3eabcc13f7b696cc8dd1597fd53df7
katarinakisi <145085697+katarinakisi@users.noreply.github.com>
2025-10-14 09:40:57 +0900

    増井瑠樹

--- STEEL BREAK/Assets/Scripts/MainMenu/MainMenu.cs
+++ STEEL BREAK/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class Briefing : MonoBehaviour
 {
@@ -13,26 +14,56 @@ public class Briefing : MonoBehaviour
     void Start()
     {
         UpdateMenu();
-        GameData.currentSelected = null; 
+        GameData.currentSelected = null;
+
+        // 各メニューにマウスイベントを登録
+        for (int i = 0; i < menuItems.Length; i++)
+        {
+            int index = i;
+            // イベントトリガーを追加（存在しない場合）
+            EventTrigger trigger = menuItems[i].gameObject.GetComponent<EventTrigger>();
+            if (trigger == null)
+            {
+                trigger = menuItems[i].gameObject.AddComponent<EventTrigger>();
+            }
+
+            // ホバー時の処理
+            EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+            entryEnter.eventID = EventTriggerType.PointerEnter;
+            entryEnter.callback.AddListener((eventData) =>
+            {
+                currentIndex = index;
+                UpdateMenu();
+            });
+            trigger.triggers.Add(entryEnter);
+
+            // クリック時の処理
+            EventTrigger.Entry entryClick = new EventTrigger.Entry();
+            entryClick.eventID = EventTriggerType.PointerClick;
+            entryClick.callback.AddListener((eventData) =>
+            {
+                currentIndex = index;
+                SelectMenu();
+            });
+            trigger.triggers.Add(entryClick);
+        }
     }
 
     void Update()
     {
-        // 上キー or W
+        // キーボード操作にも対応
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
             currentIndex = (currentIndex - 1 + menuItems.Length) % menuItems.Length;
             UpdateMenu();
         }
 
-        // 下キー or S
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
             currentIndex = (currentIndex + 1) % menuItems.Length;
             UpdateMenu();
         }
 
-        // 決定キー（Enter または Z）
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Z))
         {
             SelectMenu();
