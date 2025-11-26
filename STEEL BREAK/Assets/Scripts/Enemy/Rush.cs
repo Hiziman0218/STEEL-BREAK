4b6791b1240cd4608080f0fbd0392d816dddf1b8
Hiziman0218 <145085758+Hiziman0218@users.noreply.github.com>
2025-11-26 14:54:57 +0900

    政木陽向
    突進攻撃追加

---
+++ STEEL BREAK/Assets/Scripts/Enemy/Rush.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rush : MonoBehaviour
+{
+    [SerializeField] private Collider m_collider;           //当たり判定に使用するコライダー
+    [SerializeField] private float m_knockbackPower = 3.0f; //相手を跳ね飛ばす力
+    [SerializeField] private float m_damage = 3.0f;         //与えるダメージ
+    [SerializeField] private float m_damageInterval = 0.3f; //ダメージ間隔
+
+    private string m_myTeam; //自身の所属するチーム
+    private readonly Dictionary<CharaBase, float> m_hitTimer = new(); //ヒット管理用
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //キャラクターでなければ以降の処理を行わない
+        var chara = other.GetComponentInParent<CharaBase>();
+        if (chara == null) return;
+
+        //所属チームが同じなら以降の処理を行わない
+        if (chara.GetTeam() == m_myTeam) return;
+
+        //相手を吹き飛ばす
+        Rigidbody rb = other.GetComponentInParent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 dir = (chara.transform.position - transform.position).normalized;
+            rb.AddForce(dir * m_knockbackPower, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //自身は除外対象
+        if (other == m_collider) return;
+
+        //キャラクター確認
+        var chara = other.GetComponentInParent<CharaBase>();
+        if (chara == null) return;
+
+        //同チームは無視
+        if (chara.GetTeam() == m_myTeam) return;
+
+        float now = Time.time;
+
+        //初回 or 前回ダメージから一定時間経過したら
+        if (!m_hitTimer.ContainsKey(chara) || now - m_hitTimer[chara] >= m_damageInterval)
+        {
+            //ダメージ処理
+            chara.GetDamage(m_damage);
+            //タイマー更新
+            m_hitTimer[chara] = now;
+        }
+    }
+
+    /// <summary>
+    /// 突進攻撃開始
+    /// </summary>
+    public void StartRush()
+    {
+        m_collider.enabled = true;
+    }
+
+    /// <summary>
+    /// 突進攻撃終了
+    /// </summary>
+    public void EndRush()
+    {
+        m_collider.enabled = false;
+        m_hitTimer.Clear();
+    }
+
+    /// <summary>
+    /// 所属チームを設定
+    /// </summary>
+    /// <param name="team"></param>
+    public void SetTeam(string team)
+    {
+        m_myTeam = team;
+    }
+}
--- STEEL BREAK/Assets/Scripts/INOUE/MouseLookVertical.cs
+++ STEEL BREAK/Assets/Scripts/INOUE/MouseLookVertical.cs
@@ -16,7 +16,7 @@ public class MouseLookVertical : MonoBehaviour
     public Transform m_CameraTransform; // カメラのTransform
 
     [Header("カメラの基準位置")]
-    public Vector3 cameraBaseLocalOffset = new Vector3(0f, 1.5f, -4f); // プレイヤーから見たカメラの初期オフセット
+    public Vector3 cameraBaseLocalOffset = new Vector3(0f, 2.5f, -5f); // プレイヤーから見たカメラの初期オフセット
 
     private float currentYOffset = 0f;
 
